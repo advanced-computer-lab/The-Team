daa918f245b2b15d1720a1d3f0beb27a6857b045
ZiadAbdelmoniem <ziadearth@gmail.com>
2021-11-04 20:15:42 +0200

    Update flights.js

--- airline-system/backend/Routes/flights.js
+++ airline-system/backend/Routes/flights.js
@@ -4,14 +4,21 @@ let Flight = require('../models/flights.model');
 router.route('/').get((req, res)=>{
     Flight.find()
     .then(flights => res.json(flights))
-    .catch(err => res.status(400).json('Error: ' +err));
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res)=>{
-const from = req.body.from;
+const From = req.body.From;
+const To = req.body.To;
+const Flight_date = DATE.parse(req.body.Flight_date);
+const Cabin = req.body.Cabin;
+const Seats = Number(req.body.Seats);
 
-const to = req.body.to;
+const newFlight =new Flight({From,To,Flight_date,Cabin,Seats})
 
-const flight_date
-
+Flight.save()
+.then(()=>res.json('Flight Added!'))
+.catch(err=> res.status(400).json('Error '+err));
 });
+
+module.exports= router;
