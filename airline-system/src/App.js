0bbefc23bc9cfe2f26f4a41ae435985b2c1e3a9c
ZiadAbdelmoniem <ziadearth@gmail.com>
2021-11-06 00:38:30 +0200

    empty component created

--- airline-system/src/App.js
+++ airline-system/src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ReactDOM from 'react-dom';
+
+
+import DeleteFlight from './Components/DeleteFlight';
+
+const DeleteFlightElement = DeleteFlight();
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+      <Router>
+        <Routes>
+      <Route
+     exact path='/remove' element={DeleteFlightElement} />
+      </Routes>
+      </Router>
+
+
+
     </div>
   );
 }
---
+++ airline-system/src/Components/DeleteFlight.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function DeleteFlight() {
+    return (
+        <div>
+            <h1>yarab nekhlas</h1>
+        </div>
+    )
+}
