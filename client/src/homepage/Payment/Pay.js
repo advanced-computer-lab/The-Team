c9e0061dabedb038565b64ff1fffde1535c77aa6
zombie879 <ahmodypsp@yahoo.com>
2021-12-24 00:19:13 +0200

    login page styling

--- client/src/homepage/Payment/Pay.js
+++ client/src/homepage/Payment/Pay.js
@@ -3,7 +3,8 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from '@stripe/react-stripe-js';
 
 import CheckoutForm from "./CheckoutForm";
-import "./Appp.css";
+//import "./Appp.css";
+
 
 // Make sure to call loadStripe outside of a component’s render to avoid
 // recreating the Stripe object on every render.
