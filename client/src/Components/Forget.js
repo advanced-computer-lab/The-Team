8c5e04549a8831e929f1ed3d5b5d831fc027bb56
Abdelrahman Badran <badrannn90@gmail.com>
2021-12-25 23:49:08 +0200

    forget pass

--- client/src/Components/Forget.js
+++ client/src/Components/Forget.js
@@ -2,18 +2,27 @@ import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import axios from "axios";
-
+import { useNavigate } from "react-router-dom";
 
 export default function Forget() {
+  const navigate = useNavigate();
+
   const [mail, setMail] = useState("");
 
   const handleMail = (event) => {
     setMail(event.target.value);
   };
   const handleReset = () => {
-      axios.post("http://localhost:5000/users/forget", {email: mail})
-
-  }
+    axios
+      .post("http://localhost:5000/users/forget", { email: mail })
+      .then((res) => {
+        alert("check your email");
+        navigate("/signin");
+      })
+      .catch((err) => {
+        alert("incorrect email");
+      });
+  };
   return (
     <div>
       <br></br>
