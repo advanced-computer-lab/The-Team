22f8101cfabefbb04b5063c04f760e1afcc9027e
ZiadAbdelmoniem <ziadearth@gmail.com>
2021-12-26 00:22:54 +0200

    create flight

--- client/src/Components/CreateFlight.js
+++ client/src/Components/CreateFlight.js
@@ -1,193 +1,296 @@
-import React, { Component } from 'react';
-import '../App.css';
-import axios from 'axios';
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import axios from "axios";
+import "../App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+function CreateFlight(props) {
+  const [From, setFrom] = useState("");
+  const [To, setTo] = useState("");
+  const [Dep_date, setDep_date] = useState("");
+  const [Arr_date, setArr_date] = useState("");
+  const [Dep_time, setDep_time] = useState("");
+  const [Arr_time, setArr_time] = useState("");
+  const [Economy_seats, setEconomy_seats] = useState("");
+  const [Business_seats, setBusiness_seats] = useState("");
+  const [First_seats, setFirst_seats] = useState("");
+  const [Flight_no, setFlight_no] = useState("");
+  const [Baggage_allowance, setBaggage_allowance] = useState("");
+  const [Price, setPrice] = useState("");
+  const [Dep_terminal, setDep_terminal] = useState("");
+  const [Arr_terminal, setArr_terminal] = useState("");
+  const [Trip_duration, setTrip_duration] = useState("");
 
 
-class CreateFlight extends Component {
-  constructor(props) {
-    super(props);
-
-
-
-
-    this.onChangeFrom = this.onChangeFrom.bind(this);
-    this.onChangeTo = this.onChangeTo.bind(this);
-    this.onChangeFlight_date = this.onChangeFlight_date.bind(this);
-    this.onChangeEconomy_seats = this.onChangeEconomy_seats.bind(this);
-    this.onChangeBusiness_seats = this.onChangeBusiness_seats.bind(this);
-    this.onChangeFirst_seats = this.onChangeFirst_seats.bind(this);
-    
-    
-    
-    
-    this.onSubmit = this.onSubmit.bind(this);
-
-
-
-    this.state = {
-      From: '',
-      To:'',
-      Flight_date:'',
-      Economy_seats:'',
-      Business_seats:'',
-      First_seats:''
-    };
-  }
-
-
-
-  onChangeFrom(e) {
-    this.setState({
-      From: e.target.value
-    })
-  }
-
-  onChangeTo(e) {
-    this.setState({
-      To: e.target.value
-    })
-  }
-
-  onChangeFlight_date(e) {
-    this.setState({
-      Flight_date: e.target.value
-    })
-  }
-
-  onChangeEconomy_seats(e) {
-    this.setState({
-      Economy_seats: e.target.value
-    })
-  }
-
-  onChangeBusiness_seats(e) {
-    this.setState({
-      Business_seats: e.target.value
-    })
-  }
-
-  onChangeFirst_seats(e) {
-    this.setState({
-      First_seats: e.target.value
-    })
-  }
-
-
-
-
-  onChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+  const handleFrom = (event) => {
+    setFrom(event.target.value);
+  };
+  const handleTo = (event) => {
+    setTo(event.target.value);
+  };
+  const handleDep_date = (event) => {
+    setDep_date(event.target.value);
+  };
+  const handleArr_date = (event) => {
+    setArr_date(event.target.value);
+  };
+  const handleDep_time = (event) => {
+    setDep_time(event.target.value);
+  };
+  const handleArr_time = (event) => {
+    setArr_time(event.target.value);
+  };
+  const handleEconomy = (event) => {
+    setEconomy_seats(event.target.value);
+  };
+  const handleBusiness = (event) => {
+    setBusiness_seats(event.target.value);
+  };
+  const handleFirst = (event) => {
+    setFirst_seats(event.target.value);
+  };
+  const handleFlightno = (event) => {
+    setFlight_no(event.target.value);
+  };
+  const handleBaggage = (event) => {
+    setBaggage_allowance(event.target.value);
+  };
+  const handlePrice = (event) => {
+    setPrice(event.target.value);
+  };
+  const handleDep_terminal = (event) => {
+    setDep_terminal(event.target.value);
+  };
+  const handleArr_terminal = (event) => {
+    setArr_terminal(event.target.value);
+  };
+  const handleDuration = (event) => {
+    setTrip_duration(event.target.value);
   };
 
-  onSubmit = e => {
-    e.preventDefault();
+  const onSubmit = () => {
+
 
     const data = {
-    From: this.state.From,
-      To: this.state.To,
-      Flight_date: this.state.Flight_date,
-      Economy_seats: this.state.Economy_seats,
-      Business_seats: this.state.Business_seats,
-      First_seats: this.state.First_seats
-    };
+      From: From,
+      To: To,
+      Dep_date: Dep_date,
+      Arr_date: Arr_date,
+      Dep_time: Dep_time,
+      Arr_time: Arr_time,
+      Economy_seats: Economy_seats,
+      Business_seats: Business_seats,
+      First_seats: First_seats,
+      Flight_no: Flight_no,
+      Baggage_allowance: Baggage_allowance,
+      Price: Price,
+      Dep_terminal: Dep_terminal,
+      Arr_terminal:Arr_terminal ,
+      Trip_duration:  Trip_duration,
+    }
+
 
     axios
-      .post('http://localhost:5000/flights/add', data)
-      .then(res => {
-        this.setState({
-          From: '',
-          To:'',
-          Flight_date:'',
-          Economy_seats:'',
-          Business_seats:'',
-          First_seats:''
-        })
-        window.location.reload();
-      })
-      .catch(err => {
-          //console.log(data)
-        console.log("Error in CreateFlight!" + err);
-      })
+    .post('http://localhost:5000/flights/add', data)
+
+    .then(res=>{
+      console.log("success")
+      alert("flight added successfully");
+    })
+
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  render() {
-    return (
-    <div>
-      <h3>Create New Flight Log</h3>
-      <form onSubmit={this.onSubmit}>
+  return (
+    <div className="AddFlight">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-8 m-auto">
+            <br />
+          </div>
+          <div className="col-md-8 m-auto">
+            <h1
+              style={{
+                color: "rgb(51,51,51)",
+              }}
+            >
+              Create Flight
+            </h1>
+          </div>
+        </div>
 
-      <div className="form-group"> 
-          <label>From: </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={this.state.From}
-              onChange={this.onChangeFrom}
+        <div
+          className="col-md-8 m-auto"
+          style={{
+            display: "flex",
+            gap: 24,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <form
+            noValidate
+            onSubmit={onSubmit}
+            style={{
+              display: "grid",
+              border: "1px solid grey",
+              padding: "24px 12px",
+              borderRadius: 12,
+              boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+              background: "white",
+              gridTemplateColumns: "300px 300px",
+              gap: 12,
+            }}
+          >
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="From"
+                variant="outlined"
+                onChange={handleFrom}
               />
-        </div>
+            </div>
 
-
-        <div className="form-group"> 
-          <label>To: </label>
-          <input  type="text"
-              required
-              className="form-control"
-              value={this.state.To}
-              onChange={this.onChangeTo}
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="To"
+                variant="outlined"
+                onChange={handleTo}
               />
-        </div>
+            </div>
 
-
-        <div className="form-group">
-          <label>Flight Date:</label>
-          <input 
-              type="date" 
-              className="form-control"
-              value={this.state.Flight_date}
-              onChange={this.onChangeFlight_date}
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Dep date"
+                variant="outlined"
+                onChange={handleDep_date}
               />
-        </div>
+            </div>
 
-        <div className="form-group">
-          <label>Number of Economy Seats</label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.Economy_seats}
-              onChange={this.onChangeEconomy_seats}
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Arr date"
+                variant="outlined"
+                onChange={handleArr_date}
               />
-          
-        </div>
+            </div>
 
-        <div className="form-group">
-          <label>Number of Business Seats</label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.Business_seats}
-              onChange={this.onChangeBusiness_seats}
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Dep Time"
+                variant="outlined"
+                onChange={handleDep_time}
               />
-          
-        </div>
+            </div>
 
-        <div className="form-group">
-          <label>Number of First Seats</label>
-          <input 
-              type="text" 
-              className="form-control"
-              value={this.state.First_seats}
-              onChange={this.onChangeFirst_seats}
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Arr Time"
+                variant="outlined"
+                onChange={handleArr_time}
               />
-          
-        </div>
+            </div>
 
-        <div className="form-group">
-          <input type="submit" value="Create Flight Log" className="btn btn-primary" />
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Economy"
+                variant="outlined"
+                onChange={handleEconomy}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Business"
+                variant="outlined"
+                onChange={handleBusiness}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="First"
+                variant="outlined"
+                onChange={handleFirst}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Flightno"
+                variant="outlined"
+                onChange={handleFlightno}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Baggage allowance"
+                variant="outlined"
+                onChange={handleBaggage}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Price"
+                variant="outlined"
+                onChange={handlePrice}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Dep_terminal"
+                variant="outlined"
+                onChange={handleDep_terminal}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Arr_terminal"
+                variant="outlined"
+                onChange={handleArr_terminal}
+              />
+            </div>
+
+            <div className="form-group">
+              <TextField
+                id="outlined-basic"
+                label="Trip_duration"
+                variant="outlined"
+                onChange={handleDuration}
+              />
+            </div>
+
+
+          </form>
+          <Button onClick={onSubmit} variant="contained">
+            submit
+          </Button>
         </div>
-      </form>
+      </div>
     </div>
-    )
-  }
+  );
 }
 
 export default CreateFlight;
